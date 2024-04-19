a56beb23acda972566cc24334f13be5091479a51
Nico Loreto <103390314+NicoLoreto@users.noreply.github.com>
2024-04-19 16:42:19 -0300

    Added serialize and deserialize with json

--- fundamentals_in_CSharp/Program.cs
+++ fundamentals_in_CSharp/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 using fundamentals_in_CSharp.Models;
 using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace fundamentals_inCSharp
 {
@@ -8,39 +10,56 @@ namespace fundamentals_inCSharp
     {
         static void Main(string[] args)
         {
-            // Insertamos una cerveza.
-            CervezaBD cervezaBD = new CervezaBD();
-
-            Cerveza cerveza = new Cerveza(1, "Rubia");
-            cerveza.Marca = "Quilmes";
-            cerveza.Alcohol = 9;
-
-            cervezaBD.Add(cerveza);
-
-            // Obtenemos todas las cervezas.
-            var cervezas = cervezaBD.Get();
-
-            foreach (var c in cervezas)
+            /*
             {
-                Console.WriteLine(c.Nombre);
+                // Insertamos una cerveza.
+                CervezaBD cervezaBD = new CervezaBD();
+
+                Cerveza cerveza = new Cerveza(1, "Rubia");
+                cerveza.Marca = "Quilmes";
+                cerveza.Alcohol = 9;
+
+                cervezaBD.Add(cerveza);
+
+                // Obtenemos todas las cervezas.
+                var cervezas = cervezaBD.Get();
+
+                foreach (var c in cervezas)
+                {
+                    Console.WriteLine(c.Nombre);
+                }
+
+                // Editamos una cerveza.
+                Cerveza cerveza2 = new Cerveza(1, "Morocha");
+                cerveza.Marca = "Quilmes Stout";
+                cerveza.Alcohol = 12;
+
+                cervezaBD.Edit(cerveza2, 1);
+
+                // Eliminamos una cerveza.
+                cervezaBD.Delete(1);
             }
+            */
 
-            // Editamos una cerveza.
-            Cerveza cerveza2 = new Cerveza(1, "Morocha");
-            cerveza.Marca = "Quilmes Stout";
-            cerveza.Alcohol = 12;
-
-            cervezaBD.Edit(cerveza2, 1);
-
-            // Eliminamos una cerveza.
-            cervezaBD.Delete(1);
-
-        }
-
+        // Usando interfaces.
         static void MostrarRecomendacion(IBebidaAlcoholica bebidaAlcoholica)
         {
             bebidaAlcoholica.MaxRecomendado();
         }
-      
+
+        // Usando serialización.
+        var Cerveza = new Cerveza(10, "Cerveza");
+         
+        string miJson = JsonSerializer.Serialize(Cerveza);
+
+        // Guardar los atos del json en un archivo de texto.
+        File.WriteAllText("objeto.txt", miJson);
+
+            // Cargar el archivo y obtener objeto.
+            string json = File.ReadAllText("objeto.txt");
+
+            Cerveza cerveza = JsonSerializer.Deserialize<Cerveza>(json);
+
+        }
     }
 }
