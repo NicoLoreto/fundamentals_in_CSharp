5b2a365bba2dc8fa07576b7d9738478f3544a5f4
Nico Loreto <103390314+NicoLoreto@users.noreply.github.com>
2024-05-24 17:02:56 -0300

    Added Rest

---
+++ Rest/Models/Petitions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace Rest.Models
+{
+    class Petitions
+    {
+        public async Task GetPetition()
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+            HttpClient client = new HttpClient();
+
+            // Crea un hilo hasta await.
+            var httpResponse = await client.GetAsync(url);
+
+            Console.WriteLine("Algo");
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var res = await httpResponse.Content.ReadAsStreamAsync();
+
+                List<Post> des = JsonSerializer.Deserialize<List<Post>>(res);
+
+            }
+
+        }
+
+        public async Task PostPetition()
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts/99";
+            HttpClient client = new HttpClient();
+
+            Post post = new Post()
+            {
+                userId = 1,
+                title = "asd",
+                body = "asd"
+            };
+
+            var data =  JsonSerializer.Serialize<Post>(post);
+
+            HttpContent content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+
+            var httpResponse = await client.PostAsync(url, content);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var result = await httpResponse.Content.ReadAsStreamAsync(); 
+
+                var postResult = JsonSerializer.Deserialize<Post>(result);
+
+            }
+
+
+        }
+
+    }
+
+}
---
+++ Rest/Program.cs
@@ -0,0 +1,20 @@
+﻿using Rest.Models;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        var get = new Petitions();
+        var petitionGet = get.GetPetition();
+
+        var post = new Petitions();
+        var petitionPost = post.PostPetition();
+
+
+    }
+
+}
