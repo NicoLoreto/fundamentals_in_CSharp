7d96f34596e2cefb7c7dc93ef1ff42ffda2e8f80
Nico Loreto <103390314+NicoLoreto@users.noreply.github.com>
2024-06-14 15:35:12 -0300

    Added delegates

---
+++ Delegates/Delegate.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace Delegates
+{
+    public delegate void Imprimir<T>(T value);
+    internal class Delegate
+    {
+        public void ImprimirPantalla(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void ImprimirEntero(int value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void ExampleDelegate()
+        {
+            Imprimir<string> imprimirDel = new Imprimir<string>(ImprimirPantalla);
+            Imprimir<int> imprimirEntero = new Imprimir<int>(ImprimirEntero);
+            imprimirDel("Texto");
+            imprimirEntero(5);
+        }
+
+        public void ExampleAction()
+        {
+            // PASAR UNA FUNCION POR PARAMETRO A OTRA CLASE.
+            Action<string> actionExample = ImprimirPantalla;
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,19 @@
+﻿// Modificador de acceso - delegate - tipo de retorno
+// Action: devuelve void siempre. Func: Recibe uno o más parámetros y devuelve un parámetro. Predicate: Devuelve true o false.
+
+namespace Delegates
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Delegate del1 = new Delegate();
+            del1.ExampleDelegate();
+        }
+
+    public void AlternativeClass(Action<string> action)
+        {
+            action("imprimir");
+        }
+    }
+}
