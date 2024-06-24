e8a91ca7dcae8b75570058712503ecfdf69c9d4a
Nico Loreto <103390314+NicoLoreto@users.noreply.github.com>
2024-06-24 15:14:02 -0300

    Added a little MVC project with background tasks

---
+++ FileData.cs
@@ -0,0 +1,14 @@
+﻿namespace ASPWorker
+{
+    internal class FileData : IFileData
+    {
+        public async Task Create(string path, int n)
+        {
+            using (var sw = new StreamWriter(path))
+            {
+                await Task.Delay(1000);
+                await sw.WriteAsync($"pato {n}");
+            }
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,37 @@
+namespace ASPWorker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private string _path = Directory.GetCurrentDirectory() + @"\Files\";
+        private int _count = 0;
+
+        private IFileData _fileData;
+
+        public Worker(ILogger<Worker> logger, IFileData fileData)
+        {
+            _logger = logger;
+            _fileData = fileData;   
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // Se ejecutará hasta que no se reciba una cancelación.
+            // Ejecutar tareas en segundo plano.
+            int n = 0;
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    if (n < 10)
+                    {
+                        await _fileData.Create($"{_path}{_count++}.txt", n);
+                        n++;
+                    }
+                }
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
